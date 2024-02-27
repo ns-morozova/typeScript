56b25d0048a56cbaa16ca52e54fe0687b8405635
Морозова Наталия <ns_morozova@mail.ru>
2024-02-27 21:08:17 +0300

    Project corrected

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -12,22 +12,24 @@ export default class Cart {
     }
 
     getCostWithoutDiscount(): number {
-        let cost: number = 0;
-        for(let i = 0; i < this._items.length; i++) {
-            cost = cost + this._items[i].price;
-        }
+        let cost: number;
+        cost = this._items.reduce((sum: number, item: Buyable) => {
+            return sum + item.price;
+        }, 0);
         return cost;
     }
 
     getCostWithDiscount(discount: number): number {
-        let cost: number = 0;
-        for(let i = 0; i < this._items.length; i++) {
-            cost = cost + this._items[i].price;
-        }
+        let cost: number = this.getCostWithoutDiscount();
         return cost - cost*discount/100;
     }
 
     del(id: number): void {
-        this._items.splice(id,1);
+        this._items = this._items.filter((item: Buyable) => {
+            if (item.id == id) {
+                return false;
+            }
+            return true;  
+        });
     }
 }
